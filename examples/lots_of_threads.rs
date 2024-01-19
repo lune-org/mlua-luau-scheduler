099d49c9e87591e45d2f93307b2626c6d7413439
Filip Tibell <filip.tibell@gmail.com>
2024-01-19 23:38:41 +0100

    Add another benchmark-like example

---
+++ examples/lots_of_threads.rs
@@ -0,0 +1,33 @@
+use std::time::Duration;
+
+use smol_mlua::{
+    mlua::prelude::{Lua, LuaResult},
+    smol::Timer,
+    Runtime,
+};
+
+const MAIN_SCRIPT: &str = include_str!("./lots_of_threads.luau");
+
+const ONE_NANOSECOND: Duration = Duration::from_nanos(1);
+
+pub fn main() -> LuaResult<()> {
+    // Set up persistent lua environment
+    let lua = Lua::new();
+    lua.globals().set(
+        "sleep",
+        lua.create_async_function(|_, ()| async move {
+            // Obviously we can't sleep for a single nanosecond since
+            // this uses OS scheduling under the hood, but we can try
+            Timer::after(ONE_NANOSECOND).await;
+            Ok(())
+        })?,
+    )?;
+
+    // Load the main script into a runtime and run it until completion
+    let rt = Runtime::new(&lua)?;
+    let main = lua.load(MAIN_SCRIPT);
+    rt.push_thread(&lua, main, ());
+    rt.run_blocking(&lua);
+
+    Ok(())
+}
